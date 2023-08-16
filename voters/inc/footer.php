663c14304b8425da3aaac4be843d17d57a4b16f4
vontedduchaithra <90314288+vontedduchaithra@users.noreply.github.com>
2023-08-16 11:46:49 +0530

    Add files via upload

---
+++ voters/inc/footer.php
@@ -0,0 +1,15 @@
+
+    <div class="row bg-black text-center text-white">
+        <div class="col-12 my-3">
+            <p>  <br />
+                Developed by Vonteddu Chaithra
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<script src="../assets/js/jquery.min.js"></script>
+<script src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
